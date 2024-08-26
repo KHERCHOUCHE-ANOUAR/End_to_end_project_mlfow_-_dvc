7513baa59706cda9ea72a5d1c3b61da1e32bb49f
Anouar Kherchouche <anouar.kherchouche@expleogroup.com>
2024-08-26 11:03:47 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Chest-Cancer-Classification-Project"
+AUTHOR_USER_NAME = "KHERCHOUCHE-ANOUAR"
+SRC_REPO = "End_to_end_project_mlfow_and_dvc"
+AUTHOR_EMAIL = "kherchoucheanouar98@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
